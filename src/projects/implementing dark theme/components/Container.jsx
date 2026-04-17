53c7188bfe51abc214fdc1e3ae52b960f5df897a
Divyanshu951 <divyanshuk9515@gmail.com>
2026-04-17 19:30:02 +0530

    fix width to full

--- src/projects/implementing dark theme/components/Container.jsx
+++ src/projects/implementing dark theme/components/Container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
   return (
-    <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed font-mono transition-all duration-150 dark:bg-neutral-400">
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed font-mono transition-all duration-150 dark:bg-neutral-400">
       {children}
     </div>
   );
