1e695727bb18848b02b5f7fde45a027aaaaa5553
Divyanshu951 <divyanshuk9515@gmail.com>
2026-04-23 23:16:40 +0530

    fix background-image syntax in SVGComponent3

--- src/projects/svgs/components/SVGComponent3.jsx
+++ src/projects/svgs/components/SVGComponent3.jsx
@@ -7,8 +7,8 @@ function SVGComponent3() {
         <div className="relative z-20 flex h-full w-full items-center justify-center rounded-[px] bg-white">
           <SVGComponent1 />
         </div>
-        <div className="absolute inset-0 h-full w-full scale-[1.5] animate-spin [background-image:conic-gradient(at_center,transparent,var(--color-blue-500)_20%,transparent_30%)]"></div>
-        <div className="absolute inset-0 h-full w-full scale-[1.5] animate-spin [background-image:conic-gradient(at_center,transparent,var(--color-red-500)_20%,transparent_30%)] [animation-delay:1.5s]"></div>
+        <div className="absolute inset-0 h-full w-full scale-[1.5] animate-spin bg-[conic-gradient(at_center,transparent,var(--color-blue-500)_20%,transparent_30%)]"></div>
+        <div className="absolute inset-0 h-full w-full scale-[1.5] animate-spin bg-[conic-gradient(at_center,transparent,var(--color-red-500)_20%,transparent_30%)] [animation-delay:1.5s]"></div>
       </div>
     </div>
   );
