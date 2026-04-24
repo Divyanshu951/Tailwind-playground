c23f3c1222807099cdef6ed963488da62d12234a
Divyanshu951 <divyanshuk9515@gmail.com>
2026-04-24 14:15:04 +0530

    use apply to experiment using tailwind utility in css files

--- src/projects/svgs/components/SVGComponent3.jsx
+++ src/projects/svgs/components/SVGComponent3.jsx
@@ -2,7 +2,7 @@ import SVGComponent1 from "./SVGComponent1";
 
 function SVGComponent3() {
   return (
-    <div className="flex size-40 items-center justify-center rounded-xl border-2 border-slate-300 shadow-xl">
+    <div className="card">
       <div className="relative size-15 overflow-hidden rounded-sm bg-neutral-100 p-0.5">
         <div className="relative z-20 flex h-full w-full items-center justify-center rounded-[px] bg-white">
           <SVGComponent1 />
